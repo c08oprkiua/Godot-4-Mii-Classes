e86c76b45846c9ac86494e767fe2fd827c74a5a3
c08oprkiua <85906487+c08oprkiua@users.noreply.github.com>
2023-11-13 10:17:53 -0700

    Add files via upload

--- miidata.h
+++ miidata.h
@@ -14,14 +14,12 @@ private:
     int BinaryToInt(TypedArray<int> binary);
     TypedArray<int> ByteToBinary(int byte);
     // Meta
-    int version;
-    bool profanity;
-    int region_lock;
-    //enum region_lock {none, JPN, USA, EUR};
+    int version = 3;
+    bool profanity = false;
+    int region_lock = 0;
     int char_set;
-    //enum char_set {JPN_USA_EUR, CHN, KOR, TWN};
     //enum origin_device {Wii = 1, DS, ThreeDS, WiiUSwitch};
-    int origin_device;
+    int origin_device = 3;
     int mii_id;
     int creation_date;
     //bool unknown;
@@ -31,7 +29,8 @@ private:
     String mac_address;
 
     //General
-    enum sex {Male, Female};
+    //enum sex {Male, Female};
+    int sex = 0;
     int birthday_month;
     int birthday_day;
     int favorite_color;
@@ -63,26 +62,49 @@ public:
     void LoadFromBuffer(PackedByteArray buffer);
 
     void WriteToBuffer();
-
+    //Meta
     void set_version(int version);
     int get_version();
-
     void set_profanity(int profanity);
     bool get_profanity();
-
     void set_region_lock(int region_lock);
     int get_region_lock();
-
     void set_char_set(int char_set);
     int get_char_set();
-
     void set_origin_device(int origin_dev);
     int get_origin_device();
+    void set_mii_id(int new_id);
+    int get_mii_id();
+    void set_creation_date(int date);
+    int get_creation_date();
+    void set_dsi(bool is_from_dsi);
+    bool get_dsi();
+    void set_special(bool is_special);
+    bool get_special();
+    void set_mac_address(String new_address);
+    String get_mac_address();
 
-
-
+    //General
+    void set_sex(int wtfcppsex);
+    int get_sex();
+    void set_birthday_month(int bdaymonth);
+    int get_birthday_month();
+    void set_birthday_day(int bdayday);
+    int get_birthday_day();
+    void set_favorite_color(int color);
+    int get_favorite_color();
     void set_favorite_mii(bool favorite);
     bool get_favorite_mii();
+    void set_mii_name(String new_name);
+    String get_mii_name();
+    void set_width(float wideness);
+    float get_width();
+    void set_height(float heightness);
+    float get_height();
+    void set_sharable(bool is_sharable);
+    bool get_sharable();
+    void set_author(String new_author);
+    String get_author();
 
     ~MiiDataResource();
 };
